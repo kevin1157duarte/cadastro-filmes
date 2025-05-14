74f77ab06137b4a543ef64bed2c205595565d92b
kevin1157duarte <kevin1157.kd@gmail.com>
2025-05-13 21:31:10 -0300

    Commit Inicial

---
+++ cadastroFilmes/cadastroFilmes/src/test/java/com/KevinStenioFLDuarte/cadastroFilmes/CadastroFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KevinStenioFLDuarte.cadastroFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
